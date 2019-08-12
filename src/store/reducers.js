f87e4d3d5f8df2fcbde2f2523a1f91883d54ea27
Yann Gouffon <github@yago-art.ch>
2019-08-12 16:53:10 +0200

    🎉 bootstrap base

---
+++ src/store/reducers.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+// import { reducers as blank } from './blank/index';
+
+const rootReducer = combineReducers({
+  // blank,
+});
+
+export default rootReducer;
